0ce2c11ee60fe3b4364aa7c2a18ebb7947ff4871
Robert Nicolae <robert_yonutz_2000@yahoo.com>
2025-05-09 12:48:02 +0300

    Separate on workspaces

--- terraform-2/infra/locals.tf
+++ terraform-2/infra/locals.tf
@@ -1,39 +1,45 @@
 locals {
-  resource_group_name = "Dev"
+  workspace_suffix = lower(terraform.workspace)
+}
+
+locals {
+  resource_group_name = "${local.workspace_suffix}-rg"
   resource_location   = "North Europe"
 
   tags = {
-    environment = "Dev"
+    environment = terraform.workspace
   }
 }
 
 locals {
-  virtual_network_name          = "dev-vnet"
+  virtual_network_name          = "${local.workspace_suffix}-vnet"
   virtual_network_address_space = ["10.0.0.0/16"]
 
-  subnet_name = ["subnet1", "subnet2", "subnet3"]
+  subnet_name = ["${local.workspace_suffix}-subnet1", "${local.workspace_suffix}-subnet2", "${local.workspace_suffix}-subnet3"]
   subnet_address_prefixes = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 locals {
-  security_group_name = "my-nsg"
+  security_group_name = "${local.workspace_suffix}-nsg"
 }
 
 locals {
-  storage_account_name = "dev123robert"
+  storage_account_name = "${local.workspace_suffix}123robert"
   container_name       = "scripts"
   blob_name            = "blob1"
 }
 
 locals {
-  network_interface_name = "dev-nic"
-  public_ip_name         = "dev-public-ip"
-  ip_configuration_name   = "dev-ip-config"
+  network_interface_name = "${local.workspace_suffix}-nic"
+  public_ip_name         = "${local.workspace_suffix}-public-ip"
+  ip_configuration_name   = "${local.workspace_suffix}-ip-config"
 }
 
 locals {
-  vm_name = "dev-vm"
+  vm_name = "${local.workspace_suffix}-vm"
   vm_size = "Standard_DS1_v2"
+  computer_name = "${local.workspace_suffix}linux"
+  vm_count = 2
 }
 
 locals {
@@ -42,12 +48,4 @@ locals {
     apt-get update -y
     apt-get install -y jq
   EOT
-}
-
-locals {
-  vm_count = 2
-}
-
-locals {
-  computer_name = "devlinux"
 }
